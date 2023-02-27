f22314758df3624f07f40152e11877b480d6c4ee
parade621 <parade621@naver.com>
2023-02-27 17:47:47 +0900

    Queue code in kotlin

--- Kotlin/DataStructure/src/main/kotlin/Basic/Queue.kt
+++ Kotlin/DataStructure/src/main/kotlin/Basic/Queue.kt
@@ -1,10 +1,22 @@
 package Basic
 
 class Queue<T> {
+
     private val items = mutableListOf<T>()
 
     fun add(item: T) = items.add(item)
     fun remove() = items.removeAt(0)
     fun peek() = items[0]
     fun isEmpty() = items.isEmpty()
+}
+
+fun main(){
+    val myQueue = Queue<String>()
+
+    myQueue.add("Hello, World!")
+    myQueue.add("안녕하세요!")
+    println(myQueue.peek())
+    myQueue.remove()
+    println(myQueue.peek())
+    println(myQueue.isEmpty())
 }
