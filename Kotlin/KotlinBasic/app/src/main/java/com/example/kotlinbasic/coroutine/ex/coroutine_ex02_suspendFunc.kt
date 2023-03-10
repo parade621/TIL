dd928c42fb5f42b925aa10878a66c841301c63ef
parade621 <parade621@naver.com>
2023-03-10 11:35:01 +0900

    coroutine day02_2

--- Kotlin/KotlinBasic/app/src/main/java/com/example/kotlinbasic/coroutine/ex/coroutine_ex02_suspendFunc.kt
+++ Kotlin/KotlinBasic/app/src/main/java/com/example/kotlinbasic/coroutine/ex/coroutine_ex02_suspendFunc.kt
@@ -15,3 +15,9 @@ suspend fun doWorld(){
     delay(1000L)
     println("World!")
 }
+
+/**
+ * launch 스코프 내에 정의되어 있는 코루틴 작업을 함수로 따로 빼려면 suspend 키워드를 사용하면 된다.
+ * suspend 함수는 일반적인 함수들처럼 코루틴 내부에서 사용할 수 있고, 코루틴 스코프 내에서만 선언 가능한
+ * suspending Function(일시중단 가능한 함수)들, 즉 delay같은 메서드들을 사용할 수 있다는 특징이 있다.
+ */
