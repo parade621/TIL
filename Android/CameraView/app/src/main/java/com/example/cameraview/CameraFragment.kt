21ba25171ded9550e41d1527dc05a45521e37e04
parade621 <parade621@naver.com>
2023-03-17 10:37:08 +0900

    Take Photo function Done

--- Android/CameraView/app/src/main/java/com/example/cameraview/CameraFragment.kt
+++ Android/CameraView/app/src/main/java/com/example/cameraview/CameraFragment.kt
@@ -1,23 +1,31 @@
 package com.example.cameraview
 
-import android.app.Activity
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.example.cameraview.databinding.FragmentCameraBinding
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import kotlin.system.exitProcess
 
@@ -30,8 +38,9 @@ class CameraFragment : Fragment() {
     }
 
     // Properties
-    private var iamgeCapture: ImageCapture? = null
+    private var imageCapture: ImageCapture? = null
     private lateinit var cameraExecutor: ExecutorService
+    private lateinit var cameraAnimationListener: AnimationListener
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,6 +48,7 @@ class CameraFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        setCameraAnumationListener()
         return binding.root
     }
 
@@ -55,10 +65,62 @@ class CameraFragment : Fragment() {
                 REQUEST_CODE_PERMISSIONS
             )
         }
+
+        binding.shutter.setOnClickListener {
+            takePhoto()
+        }
     }
 
-    private fun takePhoto() {}
+    private fun takePhoto() {
+        // 안드로이드 카메라 api2에서 제공하는 캡처 기능을 안정적으로 참조하기 위해 선언
+        val imageCapture = imageCapture ?: return
 
+        // 사진이 저장될 객체의 미디어 데이터를 생성합니다.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.KOREA)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply{
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+        // 메타데이터를 포함하는 객체를 생성합니다.
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(requireActivity().contentResolver,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues)
+            .build()
+
+        // 촬영 후 실행될 이미지 캡쳐 리스너
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(binding.root.context),
+            object : ImageCapture.OnImageSavedCallback{
+                override fun onError(e: ImageCaptureException){
+                    Log.e(TAG, "사진 촬영에 실패했습니다 %{e.message}",e)
+                }
+
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    Toast.makeText(binding.root.context,
+                    "사진을 촬영하였습니다.", Toast.LENGTH_SHORT).show()
+                    val shutterAnimation = AnimationUtils.loadAnimation(binding.root.context,
+                    R.anim.shuttur_anim)
+                    shutterAnimation.setAnimationListener(cameraAnimationListener)
+                    binding.frameLayoutShutter.apply{
+                        animation = shutterAnimation
+                        visibility = View.VISIBLE
+                        startAnimation(shutterAnimation)
+                    }
+                }
+            }
+        )
+
+    }
+
+    /**
+     * 카메라를 실행하여 previewView에 쏴줍니다.
+     */
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(binding.root.context)
 
@@ -71,12 +133,18 @@ class CameraFragment : Fragment() {
                 .also {
                     it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
                 }
-            Log.d("View", "Im on a preview!!!!!!!")
+            imageCapture = ImageCapture.Builder()
+                .build()
+
             // 후방 카메라를 디폴트로 설정
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             try {
                 // 다시 바인딩 하기 전에, 자원을 언바인딩
                 cameraProvider.unbindAll()
+
+                // 카메라에 use-case(촬영) 기능을 연결
+                cameraProvider.bindToLifecycle(
+                    viewLifecycleOwner, cameraSelector,preview,imageCapture)
 
                 //  카메라 자원 할당
                 cameraProvider.bindToLifecycle(
@@ -124,10 +192,25 @@ class CameraFragment : Fragment() {
                     Toast.LENGTH_SHORT
                 ).show()
                 parentActivity.let {
-                    it?.finishAffinity()
+                    it.finishAffinity()
                     exitProcess((0))
                 }
             }
+        }
+    }
+
+    private fun setCameraAnumationListener(){
+        cameraAnimationListener = object: Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.frameLayoutShutter.visibility = View.GONE
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+            }
+
         }
     }
 
